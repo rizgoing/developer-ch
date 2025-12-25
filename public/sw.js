b3e7683ad5a67ac7741748fc13db2a0e01e0a4e6
unknown <slvmdev66@gmail.com>
2025-12-25 21:56:05 +0300

    ЗАгружаем-с иконки 2

---
+++ public/sw.js
@@ -0,0 +1,39 @@
+// Service Worker для PWA
+const CACHE_NAME = "simple-chat-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/client.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
